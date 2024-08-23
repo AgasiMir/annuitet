8d62004a7e0648688a403448d8d4a9bf3732a3fa
Agasi Miroyan <limekick@mail.ru>
2024-08-23 17:28:26 +0400

    final commit

--- bank/urls.py
+++ bank/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from .views import percent_result
 
 urlpatterns = [
-    
+    path('', percent_result),
 ]
--- bank/views.py
+++ bank/views.py
@@ -1,3 +1,21 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from bank.forms import PercentForm
+from bank.services.utils import annuitet, proper_annuitet
+
+
+def percent_result(request):
+    if request.method == "POST":
+        form = PercentForm(data=request.POST)
+        if form.is_valid():
+            money = form.cleaned_data.get("money")
+            percent = form.cleaned_data.get("percent")
+            year = form.cleaned_data.get("year")
+
+            result = proper_annuitet(annuitet(money, percent, year))
+            return render(request, "main.html", {"form": form, "res": result})
+
+        return HttpResponse("<h4>Неверные данные, попробуйте еще раз</h4>")
+
+    return render(request, "main.html", {"form": PercentForm()})
